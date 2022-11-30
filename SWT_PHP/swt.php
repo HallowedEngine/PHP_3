f6f4522646ee8836a845c840663d90e795478400
Engine <97690693+HallowedEngine@users.noreply.github.com>
2022-11-30 07:46:39 +0000

    Add files via upload

---
+++ SWT_PHP/swt.php
@@ -0,0 +1,32 @@
+<?php
+switch($gun){
+case 1;
+echo "Pazartesi";
+echo '<img src="pzt.jpeg">';
+break;
+case 2
+echo "Salı";
+echo '<img src="sali.jpg">';
+break;
+case 3;
+echo "Çarşamba";
+echo '<img src="carsamba.jpeg">';
+break;
+case 4;
+echo "Perşembe";
+echo '<img src="persembe.jpeg">';
+break;
+case 5;
+echo "Cuma";
+echo '<img src="cuma.jpeg";
+break;
+case 6
+echo "Cumartesi";
+echo '<img src="cumartesi".jpg">';
+break;
+case 7;
+echo "Pazar";
+echo '<img src="pazar.jpeg">';
+break;
+}
+?>
